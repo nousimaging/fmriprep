c9e1b0f1c929e9467e229937979c7359261c7291
jbh1091 <joseph.humphries@firmm.io>
2023-07-21 13:20:39 -0500

    create and implement threshold interface

--- fmriprep/workflows/bold/resampling.py
+++ fmriprep/workflows/bold/resampling.py
@@ -36,7 +36,7 @@ from __future__ import annotations
 
 import typing as ty
 
-from ...interfaces.maths import StdDevVol, MeanVol, BinaryDiv, CustomApplyMask,NonZeroMean, NonZeroStDev, BinarizeVol
+from ...interfaces.maths import StdDevVol, MeanVol, BinaryDiv, CustomApplyMask,NonZeroMean, NonZeroStDev, BinarizeVol, Thresh
 
 from nipype import Function
 from nipype.interfaces import freesurfer as fs
@@ -429,7 +429,7 @@ def init_goodvoxels_bold_mask_wf(mem_gb: float, name: str = "goodvoxels_bold_mas
     )
 
     goodvoxels_thr = pe.Node(
-        fsl.maths.Threshold(),
+        Thresh(),
         name="goodvoxels_thr",
         mem_gb=mem_gb,
     )
